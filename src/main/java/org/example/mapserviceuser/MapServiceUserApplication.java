998754b715a3abef73271d15208986211c838e30
dmlwjds2 <dmlwjd0809@hanyang.ac.kr>
2026-04-06 23:44:59 +0900

    init commit

---
+++ src/main/java/org/example/mapserviceuser/MapServiceUserApplication.java
@@ -0,0 +1,13 @@
+package org.example.mapserviceuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapServiceUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapServiceUserApplication.class, args);
+    }
+
+}
