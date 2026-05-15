17d93d409c374e123ebeb907da11bb726c005a2f
dmlwjds2 <dmlwjd0809@hanyang.ac.kr>
2026-05-15 22:06:48 +0900

    init: 프로젝트 초기세팅(자바 17 버전)

---
+++ src/test/java/map/service_user/ServiceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package map.service_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
